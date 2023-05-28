3640906166330f3f68c782106ea7b5378e54a7c3
joseph <staylordsxd@gmail.com>
2023-05-28 12:02:29 +0200

    Kms.

--- src/main/kotlin/com/staylords/jcollector/JCollector.kt
+++ src/main/kotlin/com/staylords/jcollector/JCollector.kt
@@ -2,8 +2,21 @@ package com.staylords.jcollector
 
 import org.bukkit.plugin.java.JavaPlugin
 
+/**
+ * @project jCollector-kotlin
+ *
+ * @date 28/05/2023
+ * @author me@staylords.com
+ */
 class JCollector : JavaPlugin() {
 
+    override fun onEnable() {
+        instance = this
+    }
 
+    companion object {
 
+        lateinit var instance: JCollector
+
+    }
 }
